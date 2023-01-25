c74fd0c8e4ef9592c14747bc0065f16af60c59ca
[onnetsystems] park saehee <pshee23@onnetsystems.net>
2023-01-25 13:37:44 +0900

    first commit

---
+++ src/test/java/com/ppc/ffs/FfsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ppc.ffs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FfsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

6a54eff4242f17d60b54030edb216964c935ac3c
[onnetsystems] park saehee <pshee23@onnetsystems.net>
2023-02-20 10:23:47 +0900

    [UPDATE] Valid check 추가 (+TODO: Valid error return 생성 기능 개발 필요)

--- src/main/java/com/ppc/ffs/branch/adapter/in/web/form/BranchUpdateRequest.java
+++ src/main/java/com/ppc/ffs/branch/adapter/in/web/form/BranchUpdateRequest.java
@@ -1,16 +1,28 @@
 package com.ppc.ffs.branch.adapter.in.web.form;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+
 import com.ppc.ffs.branch.domain.BranchModifyInfo;
 
 import lombok.Data;
 
 @Data
 public class BranchUpdateRequest {
+	@NotNull
+	private Long branchId;
+	
+	@NotBlank
 	private String name;
+	
+	@NotBlank
 	private String address;
+	
+	@Pattern(regexp = "^\\d{2,3}-\\d{3,4}-\\d{4}$")
 	private String phoneNumber;
 	
-	public BranchModifyInfo of(Long branchId) {
+	public BranchModifyInfo of() {
 		return BranchModifyInfo.builder()
 				.branchId(branchId)
 				.name(name)
